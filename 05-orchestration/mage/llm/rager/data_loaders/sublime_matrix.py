fb6e5a865e29f2b72680b2d2d58c8597d59c246a
BastetK <94530879+BastetK@users.noreply.github.com>
2024-08-20 02:05:37 +0000

    hw5

---
+++ 05-orchestration/mage/llm/rager/data_loaders/sublime_matrix.py
@@ -0,0 +1,69 @@
+from typing import Dict, List, Union
+
+import numpy as np
+from elasticsearch import Elasticsearch
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def search(query_embedding: Union[List[int], np.ndarray], *args, **kwargs) -> List[Dict]:
+    connection_string = kwargs.get('connection_string', 'http://localhost:9200')
+    '''
+    index_name = kwargs.get('index_name', 'documents')
+    source = kwargs.get('source', "cosineSimilarity(params.query_vector, 'embedding') + 1.0")
+    top_k = kwargs.get('top_k', 5)
+    chunk_column = kwargs.get('chunk_column', 'content')
+
+    if isinstance(query_embedding, np.ndarray):
+        query_embedding = query_embedding.tolist()
+
+    script_query = dict(
+        script_score=dict(
+            query=dict(match_all=dict()),
+            script=dict(source=source, params=dict(query_vector=query_embedding)),
+        )
+    )
+'''
+    es_client = Elasticsearch(connection_string)
+'''
+    response = es_client.search(
+        index=index_name,
+        query=dict(
+            size=top_k,
+            query=script_query,
+            _source=[chunk_column],
+        ),
+    )
+'''
+    script_query=dict(
+        bool=dict(
+            must=dict(
+                multi_match=dict(
+                    query="When is the next cohort?",
+                    fields=list("question^4", "text"),
+                    type="best_fields",
+                )
+            )
+        )
+
+    search_query = dict(
+        size=5,
+        query=script_query,
+        ),
+    )
+
+    response = es_client.search(index='documents_20240820_1522', query=search_query)
+
+    return [hit['_source']['content'] for hit in response['hits']['hits']]
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
